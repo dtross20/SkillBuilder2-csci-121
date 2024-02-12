2f7258b4b75a084c00fb4068b5783a3e10c7af87
deyvon <dtross20@students.desu.edu>
2024-02-12 16:32:59 -0500

    Completed1

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -4,35 +4,65 @@
  * a game.  The purpose of a power pill is to simply
  * provide a character with power.
  *
- * @author (You)
+ * @author (Deyvon)
  * @version (0.1)
  */
-public class PowerPill
-{
-    // TODO - replace this line with instruction from step 1
+public class PowerPill {
+    private static final int DEFAULT_POWER = 10;
 
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    private int power;
+    private String name;
+
 
     // constructors
 
-    // TODO - replace this line with instruction from step 4
+    /**
+     * \* Initializes this power pill to a default power value
+     * \* Sets the name of the pill to name.
+     * \* @param name the name of this power pill.
+     */
 
-    // TODO - replace this line with instruction from step 5
+    public PowerPill(String name) {
+        this.name = name;
+        power = DEFAULT_POWER;
+    }
+
+    /**
+     * Initializes this power pill to the value of power and sets the name of the pill to name.
+     *
+     * @param name  is the name of this power pill
+     * @param power is the power level of this power pill.
+     */
+
+    public PowerPill(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
 
 
     // accessor methods
 
-    // TODO - replace this line with instruction from step 6
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
 
 
     // mutator methods
+    public void setPower(int power) {
+        this.power = power;
+    }
 
-    // TODO - replace this line with instruction from step 7
+    public void setName(String name) {
+        this.name = name;
+    }
 
     // toString method
-
-    // TODO - replace this line with instruction from step 8
-
+    public String toString() {
+        return "PowerPill " + name + " = " + power;
+    }
 }
